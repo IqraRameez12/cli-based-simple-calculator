8edb51197a0b0b47b475b00005e611de2d71607b
iqra <161558082+IqraRameez12@users.noreply.github.com>
2024-05-31 11:57:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "FirstNumber",
+        message: "Enter your first number",
+        type: "number"
+    },
+    {
+        name: "secondNumber",
+        type: "number",
+        message: "Enter your secondNumber"
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Select your operator to perform operation",
+        choices: ["+", "-", "*", "/"]
+    }
+]);
+//conditional statment:
+if (answer.operator === "+") {
+    console.log(answer.FirstNumber + answer.secondNumber);
+}
+else if (answer.operator === "-") {
+    console.log(answer.FirstNumber - answer.secondNumber);
+}
+else if (answer.operator === "*") {
+    console.log(answer.FirstNumber * answer.secondNumber);
+}
+else if (answer.operator === "/") {
+    console.log(answer.FirstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select a valid number");
+}
